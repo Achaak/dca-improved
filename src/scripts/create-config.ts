ac8d90abe30079137791476b1bc5221ced8c3e3d
Axel Lavoie <axel.lavoie@gmail.com>
2025-03-02 16:59:55 +0100

    feat: update command line argument handling to directly modify defaultConfig properties

--- src/scripts/create-config.ts
+++ src/scripts/create-config.ts
@@ -15,26 +15,22 @@ const defaultConfig: Config = {
 const args = Bun.argv.slice(2);
 const configName = getConfigName(args, "-n") ?? "config";
 
-let token = defaultConfig.token;
-let startDate = defaultConfig.start_date;
-let endDate = defaultConfig.end_date;
-
 // Process command line arguments
 args.forEach((arg, index) => {
   switch (arg) {
     case "-t":
       if (index + 1 < args.length) {
-        token = args[index + 1];
+        defaultConfig.token = args[index + 1];
       }
       break;
     case "-s":
       if (index + 1 < args.length) {
-        startDate = args[index + 1];
+        defaultConfig.start_date = args[index + 1];
       }
       break;
     case "-e":
       if (index + 1 < args.length) {
-        endDate = args[index + 1];
+        defaultConfig.end_date = args[index + 1];
       }
       break;
   }
